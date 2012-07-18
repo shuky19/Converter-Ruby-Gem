4a44e8e66bea301bc3c01845eddeb569b99d1c37
Shuky Chen <shukyc19@gmail.com>
2012-07-19 00:43:24 +0300

    add remarks

--- Converter.rb
+++ Converter.rb
@@ -1,17 +1,26 @@
+# This module helps converting between to instances
+# of different classes
 module Converter
   def self.included(base)
     base.extend(ClassConverter)
   end
 
   def self.convert(source, target_type)
+    # Create new target instance
     target = target_type.new
+
+    # Gets source Conversion definition
     conversion_properties = source.class.class_eval { @attribute_converter}
 
+    # update each accessor on the target according to the conversion definition
     source.instance_variables.each do |var|
       var_name = var.to_s.delete('@').to_sym
-      if conversion_properties.has_key? var_name
-        convert_property = conversion_properties[var_name]
+
+      # Get the conversion definition for the current accessor if exists
+      if convert_property = conversion_properties[var_name]
         target_property_name = convert_property.target_property_name.to_s.concat('=').to_sym
+
+        # Convert from one type to another (by default doesn't do anything)
         target_value = convert_property.convert_block.call(source.send(var_name))
         target.send(target_property_name, target_value)
       end
@@ -21,14 +30,21 @@ module Converter
   end
 
   def self.convertBack(target, source_type)
+    # Create new target instance
     source = source_type.new
+
+    # Gets source Conversion definition
     conversion_properties = source.class.class_eval { @attribute_converter }
 
+    # update each accessor on the target according to the conversion definition
     source.methods.grep(/[a-zA-Z0-9]=$/).each do |var_name|
       var_name_trimmed = var_name.to_s.delete('=').to_sym
-      if conversion_properties.has_key? var_name_trimmed
-        convert_property = conversion_properties[var_name_trimmed]
+
+      # Get the conversion definition for the current accessor if exists
+      if convert_property = conversion_properties[var_name_trimmed]
         target_property_name = convert_property.target_property_name
+
+        # Convert from one type to another (by default doesn't do anything)
         source_value = convert_property.convert_back_block.call(target.send(target_property_name))
         source.send(var_name, source_value)
       end
@@ -37,18 +53,23 @@ module Converter
     source
   end
 
+  # This module add class extension of attr_converter
   module ClassConverter
     def attr_converter(symbol, attr_another_name = nil, convert_bock = nil, convert_back_block = nil)
+      # Set default values for nil arguments
       attr_another_name ||= symbol
       convert_bock ||= lambda { |v| v }
       convert_back_block ||= lambda { |v| v }
       @attribute_converter ||= {}
-      @attribute_converter[symbol] =AttributeMetadata.new(symbol, attr_another_name, convert_bock, convert_back_block)
+
+      # Create new ConversionMetadata
+      @attribute_converter[symbol] =ConversionMetadata.new(symbol, attr_another_name, convert_bock, convert_back_block)
       attr_accessor symbol
     end
   end
 
-  class AttributeMetadata
+  # Represent conversion data of one property to another
+  class ConversionMetadata
     def initialize(source_prop_name, target_prop_name, convert, convert_back)
       @source_property_name = source_prop_name
       @target_property_name = target_prop_name
